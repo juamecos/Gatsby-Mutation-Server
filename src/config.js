a3b08932dbe44327147a276097be6c0420368954
juamecos <juamecos@gmail.com>
2020-09-28 08:36:18 +0200

    working in development

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const {
-  APP_PORT = 4000,
-  NODE_ENV = "development",
-  DB_USERNAME = "juan-chat",
-  DB_PASSWORD = "hENAYr6HaEk9X61l",
-  DB_NAME = "chat",
-} = process.env;
-
-export const IN_PROD = NODE_ENV === "production";
